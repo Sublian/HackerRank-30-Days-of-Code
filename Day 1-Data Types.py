b4498c71b40d3a55b41beaff63a26ae0cdfb0b9d
Luis Gonzalez <subliandev@gmail.com>
2023-08-16 19:48:03 -0500

    Create Day 1-Data Types.py
    Objective
    Today, we're discussing data types. Check out the Tutorial tab for learning materials and an instructional video!

---
+++ Day 1-Data Types.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Read and save an integer, double, and String to your variables.
+entero=int(input())
+decimal=float(input())
+cadena=input()
+print(i+entero)
+print(d+decimal)
+print(s+cadena)
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
