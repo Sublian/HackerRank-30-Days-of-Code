f15472d101cb2ab4d21c16cc083ac6d8097320ac
Luis Gonzalez <subliandev@gmail.com>
2023-09-08 08:39:37 -0500

    Update Day 19: Interfaces.py

--- Day 19: Interfaces.py
+++ Day 19: Interfaces.py
@@ -1,3 +1,6 @@
 class Calculator(AdvancedArithmetic):
-    def divisorSum(self, n):
-        pass
+    self.n, self.l = n, []
+    for i in range(1, self.n + 1):
+        if self.n%i == 0:
+            self.l.append(i)
+    return sum(self.l)
