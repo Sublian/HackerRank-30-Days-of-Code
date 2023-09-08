34e92f7e684e848818e93c2a13c6abadcc1bf09b
Luis Gonzalez <subliandev@gmail.com>
2023-09-08 08:44:51 -0500

    Create Day 21: Generics.py

---
+++ Day 21: Generics.py
@@ -0,0 +1,10 @@
+#funny moment... not suport python...
+#that code is in C++
+
+template <typename T>
+
+void printArray(vector<T> arr){
+    for (int i = 0; i < arr.size(); i++){
+        cout << arr[i] << "\n";
+    }
+}
